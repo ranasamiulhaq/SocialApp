876697cdf7f73c8b824674a305e38080e650602a
Rana Sami Ul Haq <rsami3638@gmail.com>
2025-10-17 11:18:24 +0500

    Tailwind Css integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './App.css';
 
 // Import BrowserRouter to provide routing context
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
